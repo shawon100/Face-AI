d878af1c2d08dd7abe98bbcac956b101f3db4e7e
Ashadullah Shawon <shawonashadullah@gmail.com>
2019-10-10 11:58:15 +0600

    Update facialexpression.js

--- facialexpression.js
+++ facialexpression.js
@@ -1,11 +1,11 @@
 const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(start)
 
 async function start() {
